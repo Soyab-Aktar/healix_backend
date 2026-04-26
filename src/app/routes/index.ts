986ddc236aaca1a8a9bcc87fd1522988da3f0985
Soyab-Aktar <iamsoyabcode23@gmail.com>
2026-04-26 08:33:56 +0530

    Doctor_Schedule api created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,9 @@ import { UserRoute } from "../module/user/user.route";
 import { DoctorRoute } from "../module/doctor/doctor.route";
 import { AdminRoute } from "../module/admin/admin.route";
 import { SuperAdminRoute } from "../module/superAdmin/superAdmin.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
+import { AppointmentRoutes } from "../module/appointment/appointment.route";
 
 const router = Router();
 
@@ -14,6 +17,9 @@ router.use("/users", UserRoute);
 router.use("/doctors", DoctorRoute);
 router.use("/admins", AdminRoute);
 router.use("/superAdmins", SuperAdminRoute);
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedules", DoctorScheduleRoutes);
+router.use("/appointments", AppointmentRoutes);
 
 
 export const IndexRoutes = router;
