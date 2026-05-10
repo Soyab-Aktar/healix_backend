74c6597f8e94a6f42192a55e4717b0cba8b8fecc
Soyab-Aktar <iamsoyabcode23@gmail.com>
2026-05-10 21:30:40 +0530

    Handle Prisma Errors , Update Global Error middleware, handle SIGTERM-SIGINT-uncaught(exception/rejection) errors

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,70 +1,100 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
-import { envVars } from "../config/env";
 import status from "http-status";
-import z, { promise } from "zod";
-import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
-import { handleZodError } from "../errorHelpers/handleZodError";
+import z from "zod";
+import { Prisma } from "../../generated/prisma/client";
+import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
-import { deleteFileFromCloudinary } from "../config/cloudinary.config";
+import { handlePrismaClientKnownRequestError, handlePrismaClientUnknownError, handlePrismaClientValidationError, handlerPrismaClientInitializationError, handlerPrismaClientRustPanicError } from "../errorHelpers/handlePrismaErrors";
+import { handleZodError } from "../errorHelpers/handleZodError";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 import { deleteUploadedFilesFromGlobalErrorHandler } from "../utils/deleteFilesFromGlobalErrorHandler";
-
 
 
 export const globalErrorHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
   if (envVars.NODE_ENV === 'development') {
     console.log("Error from Global Error Handler", err);
   }
-
-  // if (req.file) {
-  //   await deleteFileFromCloudinary(req.file.path);
-  // }
-
-  // if (req.files && Array.isArray(req.files) && req.files.length > 0) {
-  //   const imageURLs = req.files.map((file) => file.path);
-  //   await Promise.all(imageURLs.map(url => deleteFileFromCloudinary(url)));
-  // }
-
   await deleteUploadedFilesFromGlobalErrorHandler(req);
 
+  let errorSources: TErrorSources[] = []
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
   let message: string = 'Internal Server Error';
-  let errorSources: TErrorSources[] = [];
+  let stack: string | undefined = undefined;
 
-  if (err instanceof z.ZodError) {
-    const simplifiedError = handleZodError(err);
-    statusCode = simplifiedError.statusCode as number;
-    message = simplifiedError.message;
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    const simplifiedError = handlePrismaClientKnownRequestError(err);
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
     errorSources = [...simplifiedError.errorSources]
-  }
-  else if (err instanceof AppError) {
+    stack = err.stack;
+
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    const simplifiedError = handlePrismaClientUnknownError(err);
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+    stack = err.stack;
+
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    const simplifiedError = handlePrismaClientValidationError(err)
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+    stack = err.stack;
+
+  } else if (err instanceof Prisma.PrismaClientRustPanicError) {
+    const simplifiedError = handlerPrismaClientRustPanicError();
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+    stack = err.stack;
+
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    const simplifiedError = handlerPrismaClientInitializationError(err);
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+    stack = err.stack;
+
+  } else if (err instanceof z.ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode as number
+    message = simplifiedError.message
+    errorSources = [...simplifiedError.errorSources]
+    stack = err.stack;
+
+  } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
+    stack = err.stack;
     errorSources = [
       {
         path: '',
         message: err.message
       }
-    ];
+    ]
   }
   else if (err instanceof Error) {
     statusCode = status.INTERNAL_SERVER_ERROR;
-    message = err.message;
+    message = err.message
+    stack = err.stack;
     errorSources = [
       {
         path: '',
         message: err.message
       }
-    ];
+    ]
   }
+
 
   const errorResponse: TErrorResponse = {
     success: false,
     message: message,
     errorSources,
     error: envVars.NODE_ENV === 'development' ? err : undefined,
-    stack: envVars.NODE_ENV === 'development' ? err : undefined,
+    stack: envVars.NODE_ENV === 'development' ? stack : undefined,
   }
-
 
   res.status(statusCode).json(errorResponse);
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,63 @@
+import { Server } from "http";
 import app from "./app";
 import { envVars } from "./app/config/env";
 import { seedSuperAdmin } from "./app/utils/seed";
 
 
+let server: Server;
 const bootstrap = async () => {
   try {
     await seedSuperAdmin();
-    app.listen(envVars.PORT, () => {
+    server = app.listen(envVars.PORT, () => {
       console.log(`Server is running on http://localhost:${envVars.PORT}`);
     })
   } catch (error) {
     console.error('Failed to Start Server :', error);
   }
 }
+
+// SIGTERM signal error
+process.on('SIGTERM', (error) => {
+  console.log("SIGTERM Signal Received, Server Shuting down ....: ", error);
+  if (server) {
+    server.close(() => {
+      process.exit(0);
+    })
+  }
+  process.exit(0);
+})
+// SIGINT signal handler
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received. Shutting down server...");
+  if (server) {
+    server.close(() => {
+      console.log("Server closed gracefully.");
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// uncaught exception handler
+process.on('uncaughtException', (error) => {
+  console.log("Uncaught Exception Detected, Server Shuting down ....: ", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    })
+  }
+  process.exit(1);
+})
+// uncaught rejection handler
+process.on('unhandledRejection', (error) => {
+  console.log("Unhandled Rejection Detected, Server Shuting down ....: ", error);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    })
+  }
+  process.exit(1);
+})
+
 
 bootstrap();
